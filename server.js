0ba632f5e022943b3c22abd6ded515250d7d9464
daneeskripter <imdanee@seznam.cz>
2023-08-24 18:24:59 +0200

    server.js

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
 const { logSuccess, logError, logInfo, logWarn } = require("./handlers/logger");
+const path = require('path');
 
 if (process.env.NODE_ENV !== "production") {
     require("dotenv").config()
   }
 
+app.use(express.static(path.join(__dirname, "public")))
+
 app.set('view engine', 'ejs');
+
+app.get("/", function (req, res) {
+    res.render(__dirname + "/views/index.ejs", {})
+})
 
 
 app.listen(process.env.PORT)
