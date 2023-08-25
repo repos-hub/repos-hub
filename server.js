a62df3d0695b1b8c26a4bf6581bd5f85cb39979b
daneeskripter <imdanee@seznam.cz>
2023-08-25 18:06:48 +0200

    Added DB Integration

--- server.js
+++ server.js
@@ -2,19 +2,52 @@ const express = require('express');
 const app = express();
 const { logSuccess, logError, logInfo, logWarn } = require("./handlers/logger");
 const path = require('path');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const session = require('cookie-session');
+const { checkAuth, checkNotAuth} = require("./handlers/authChecks");
 
 if (process.env.NODE_ENV !== "production") {
     require("dotenv").config()
   }
 
-app.use(express.static(path.join(__dirname, "public")))
+// DATABASE
+mongoose.connect(process.env.MONGO_SRV, {
+}).then(() =>[
+  logSuccess("Connected to the database!")
+]).catch((err) =>{
+  logError('Failed connect to the database!')
+})
+
+const initializePassport = require("./handlers/passport")
+initializePassport(passport)
+app.use(session({
+  keys: [process.env.SESSION_SECRET],
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: false,
+    httpOnly: true,
+    domain: process.env.DOMAIN,
+    maxAge: 86400000
+  }
+}))
+
+app.use(express.urlencoded({ extended: false}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
 
 app.set('view engine', 'ejs');
 
 app.get("/", function (req, res) {
-    res.render(__dirname + "/views/index.ejs", {})
+    res.render(__dirname + "/views/index.ejs", {isAuthenticated: req.isAuthenticated(), user: req.user});
 })
 
+app.use("/login", require("./routes/auth").login);
+
+app.use("/logout", require("./routes/auth").logout);
 
 app.listen(process.env.PORT)
 logSuccess(`Server is listening on port ${process.env.PORT}`)
