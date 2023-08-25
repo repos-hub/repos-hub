90177a63dc7ad62b89c67a673e4b986f3e523218
daneeskripter <imdanee@seznam.cz>
2023-08-25 18:06:00 +0200

    auth routes

---
+++ routes/auth.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router()
+const router2 = express.Router()
+const passport = require("passport");
+
+router.get("/", passport.authenticate("github", { scope: [ 'user:email',  ] }))
+
+router.get("/callback", passport.authenticate("github", { failureRedirect: "/", successRedirect: "/"}))
+
+router2.post('/', function(req, res, next){
+    req.logout();
+    res.redirect('/');
+  });
+
+module.exports.login = router
+module.exports.logout = router2
+
