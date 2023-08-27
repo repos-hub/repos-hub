3aa2318ef29a81fbe36738807b85ad0f8b2257d7
daneeskripter <imdanee@seznam.cz>
2023-08-27 17:35:05 +0200

    Added Repository model

---
+++ models/Repository.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Repository = new mongoose.Schema({
+    name: {type: String, required: true},
+    owner: {type: String, required: true},
+    stars: {type: String, required: true},
+    forks: {type: String, required: true},
+    watchers: {type: String, required: true},
+    description: {type: String, required: true},
+    repoofmonth: {type: Boolean, default: false},
+    githublink: {type: String, required: true},
+    websitelink: {type: String, required: true},
+    docslink: {type: String, required: true},
+})
+
+const model = mongoose.model('repos', Repository)
+
+module.exports = model
