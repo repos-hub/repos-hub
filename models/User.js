dec4b6bbd915b79211492bbb59c5f5681d05cdf2
daneeskripter <imdanee@seznam.cz>
2023-08-27 17:34:53 +0200

    User model updated

--- models/User.js
+++ models/User.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 const User = new mongoose.Schema({
-    username: {type: String, required: true}
+    username: {type: String, required: true},
+    isAdmin: {type: Boolean, default: false},
+    email: {type: String, required: true},
 })
 
 const model = mongoose.model('users', User)
