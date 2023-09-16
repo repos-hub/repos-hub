ad44eb14c4828d5f1d210c09b3179a4d1d21afbb
daneeskripter <imdanee@seznam.cz>
2023-09-16 10:31:48 +0200

    email removed

--- models/User.js
+++ models/User.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const User = new mongoose.Schema({
     username: {type: String, required: true},
     isAdmin: {type: Boolean, default: false},
-    email: {type: String, required: true},
 })
 
 const model = mongoose.model('users', User)
